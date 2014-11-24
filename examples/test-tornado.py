cce3d23c954ed33034c17734bdb9db48b81204cc
花漫 <zhubo.zb@alibaba-inc.com>
2014-11-24 13:50:10 +0800

    sample

--- examples/test-tornado.py
+++ examples/test-tornado.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("..")
 import tornado.ioloop
 import tornado.web
@@ -9,10 +10,23 @@ class MyTornadoFans(fans.Tornado):
     app_secret = 'appsecretEnF5leY4V'
 
     def receive_text(self, text):
-        self.reply_text('123')
+        if text == '文章':
+            self.reply_articles([
+                {
+                    "display_name": "两个故事",
+                    "summary": "今天讲两个故事，分享给你。谁是公司？谁又是中国人？​",
+                    "image": "http://storage.mcp.weibo.cn/0JlIv.jpg",
+                    "url": "http://e.weibo.com/mediaprofile/article/detail?uid=1722052204&aid=983319"
+                }
+            ])
+        else:
+            self.reply_text(text)
 
     def receive_event(self, event):
         self.reply_text('event: ' + event)
+
+    def receive_default(self):
+        self.reply_text('发送『文章』，将返回文章；发送其他文字将原文返回。')
 
 
 class MainHandler(tornado.web.RequestHandler):
